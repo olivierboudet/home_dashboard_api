d4bc355e351fc54d439b5d7de321bba0f793abe2
Olivier Boudet <o.boudet@gmail.com>
2017-03-10 15:49:37 +0100

    add logs

--- src/main/java/org/boudet/home/dashboard/api/EmitterManager.java
+++ src/main/java/org/boudet/home/dashboard/api/EmitterManager.java
@@ -1,6 +1,8 @@
 package org.boudet.home.dashboard.api;
 
 import org.boudet.home.dashboard.api.enums.TypeEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -13,12 +15,26 @@ import java.util.stream.Collectors;
 
 @Service
 public class EmitterManager {
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
     private final Map<JobKey, List<SseEmitter>> emitters = new ConcurrentHashMap<>();
 
     public void register(JobKey key, SseEmitter emitter) {
         if(!emitters.containsKey(key)) {
             emitters.put(key, new ArrayList<>());
         }
+
+        emitter.onTimeout(() -> {
+            LOGGER.debug("************* Emitter Timeout !");
+            emitter.complete();
+            unregister(key, emitter);
+        });
+
+        emitter.onCompletion(() -> {
+            LOGGER.debug("************* Emitter completed");
+            emitter.complete();
+            unregister(key, emitter);
+        });
 
         List listEmitters = emitters.get(key);
         listEmitters.add(emitter);
